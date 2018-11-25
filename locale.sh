d09c264aabda3dbc2838c84860fa8609690f1240
G Panula <github@kablah.com>
2018-11-25 13:53:32 -0600

    put more config bits in /etc/profile.d to ease admin duties

---
+++ locale.sh
@@ -0,0 +1,5 @@
+# locale settings
+# http://www.gentoo.org/doc/en/guide-localization.xml
+# also see http://www.linuxquestions.org/questions/slackware-14/gnome-clock-411886/
+export LC_ALL="en_US.UTF-8"
+export LANG="en_US.UTF-8"
